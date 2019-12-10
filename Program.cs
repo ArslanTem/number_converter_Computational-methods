1f218a142c986ad943e7da58abbaf44b3fd60162
ArslanTem <38863504+ArslanTem@users.noreply.github.com>
2019-12-10 22:33:17 +0100

    Class convert

--- Program.cs
+++ Program.cs
@@ -4,12 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace number_converter
+namespace Converter
 {
     class Program
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Which Numberal system u want to input");
+            Console.WriteLine("1)Decimal");
+            Console.WriteLine("2)Binary");
+            Console.WriteLine("3)Octal");
+            Console.WriteLine("4)HexaDecimal");
+
+            int choise = int.Parse(Console.ReadLine());
+            switch (choise)
+            {
+                case 1:
+
+                    Console.WriteLine("Enter Decimal value");
+                    int decimal_value = 0;
+                    convert.decimal_value(decimal_value);
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Enter Binary value");
+                    string binary_value = "";
+                    convert.binary_value(binary_value);
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("Enter Octal value:");
+                    string octal_value = "";
+                    convert.octal_value(octal_value);
+                    break;
+
+                case 4:
+
+                    Console.WriteLine("Enter Hexa-decimal value:");
+                    string hexa_dec_value = "";
+                    convert.hexa_decimal_value(hexa_dec_value);
+                    break;
+
+
+            }
+
+
         }
+
     }
 }
+
+
---
+++ convert.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Converter
+{
+    class convert
+    {
+        public static string hexa_decimal_value(string hexa_value)
+        {
+
+            hexa_value = Console.ReadLine();
+            int int_value = Convert.ToInt32(hexa_value, 16);
+            string octal = Convert.ToString(int_value, 8);
+            string binary = Convert.ToString(int_value, 2);
+
+            Console.WriteLine("hex_value = {0}", hexa_value);
+            Console.WriteLine("Decimal_value = {0}", int_value);
+            Console.WriteLine("Binary_value = {0}", binary);
+            Console.WriteLine("Octa_value = {0}", octal);
+
+            return hexa_value;
+        }
+        public static string binary_value(string binary)
+        {
+            binary = Console.ReadLine();
+            int int_value = Convert.ToInt32(binary, 2);
+            string octal = Convert.ToString(int_value, 8);
+            string hexa_dec = Convert.ToString(int_value, 16);
+
+            Console.WriteLine("Binary_value = {0}", binary);
+            Console.WriteLine("Decimal_value = {0}", int_value);
+            Console.WriteLine("Octa_value = {0}", octal);
+            Console.WriteLine("hex_value = {0}", hexa_dec);
+
+            return binary;
+        }
+        public static string octal_value(string octal)
+        {
+            octal = Console.ReadLine();
+            int int_value = Convert.ToInt32(octal, 8);
+            string binary = Convert.ToString(int_value, 2);
+            string hexa_dec = Convert.ToString(int_value, 16);
+
+            Console.WriteLine("Octa_value = {0}", octal);
+            Console.WriteLine("Decimal_value = {0}", int_value);
+            Console.WriteLine("Binary_value = {0}", binary);
+            Console.WriteLine("hex_value = {0}", hexa_dec);
+
+
+            return octal;
+        }
+        public static int decimal_value(int int_value)
+        {
+
+            int_value = int.Parse(Console.ReadLine());
+            string octal = Convert.ToString(int_value, 8);
+            string binary = Convert.ToString(int_value, 2);
+            string hexa_value = Convert.ToString(int_value, 16);
+
+
+            Console.WriteLine("hex_value = {0}", hexa_value);
+            Console.WriteLine("Decimal_value = {0}", int_value);
+            Console.WriteLine("Binary_value = {0}", binary);
+            Console.WriteLine("Octa_value = {0}", octal);
+
+            return int_value;
+        }
+    }
+}
